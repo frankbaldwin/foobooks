e46d77646a5589142dc7c2379dac2cd7fe0b1548
Frank Baldwin <frankbaldwin@hotmail.com>
2015-11-12 09:57:53 +0700

    Seeder for book table

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,7 +14,9 @@ class DatabaseSeeder extends Seeder
     {
         Model::unguard();
 
-        // $this->call(UserTableSeeder::class);
+        $this->call(BooksTableSeeder::class);
+      //  $this->call(AuthorTableSeeder::class);
+
 
         Model::reguard();
     }
